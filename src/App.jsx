e954ea505cdebbc57971a70774af418f083706a2
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2025-09-10 12:09:28 +0300

    Create Food component and add type checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 import { Stage } from '@pixi/react'
+import { BOARD_HEIGHT, BOARD_WIDTH, GAME_COLOR, GAME_HEIGHT, GAME_WIDTH } from './config';
+import Food from './components/Food';
+
+const getRandomPosition = () => ({
+    x: Math.floor(Math.random() * BOARD_WIDTH),
+    y: Math.floor(Math.random() * BOARD_HEIGHT),
+});
 
 function App() {
-  const gameWidth = 800;
-  const gameHeight = 600;
+    const [foodPosition, setFoodPosition] = useState(getRandomPosition());
 
-  return (
-    <Stage
-      width={gameWidth}
-      height={gameHeight}
-      options={{ backgroundColor: 0x1d1d1d }}
-    >
-      {/* Our game components will go here later */}
-    </Stage>
-  )
+    return (
+        <Stage
+            width={GAME_WIDTH}
+            height={GAME_HEIGHT}
+            options={{ backgroundColor: GAME_COLOR }}
+        >
+            <Food
+                x={foodPosition.x}
+                y={foodPosition.y}
+            // x={getRandomPosition().x}
+            // y={getRandomPosition().y}
+            />
+        </Stage>
+    )
 }
 
 export default App;
---
+++ src/components/Food.jsx
@@ -0,0 +1,31 @@
+import { useCallback } from "react";
+import { FOOD_COLOR, GRID_SIZE } from "../config";
+import { Graphics } from "@pixi/react";
+import PropTypes from 'prop-types';
+
+function Food({ x, y }) {
+    // useCallback is used for performance optimization. It ensures the draw function
+    // is not recreated on every render, as its logic is static.
+    const draw = useCallback(g => {
+        g.clear();
+        g.beginFill(FOOD_COLOR);
+        g.drawRect(0, 0, GRID_SIZE, GRID_SIZE);  // Draws a square for the food
+        g.endFill();
+    }, []);
+
+    return (
+        <Graphics
+            draw={draw}
+            x={x * GRID_SIZE}
+            y={y * GRID_SIZE}
+        />
+    );
+};
+
+// Using PropTypes for type-checking is a good practice in professional React projects.
+Food.propTypes = {
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
+};
+
+export default Food;
