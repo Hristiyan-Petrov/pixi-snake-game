c0772b02af7297c5ca15ac8f22f1ca735af58afa
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2025-09-10 12:08:54 +0300

    Add config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const GRID_SIZE = 20 // Each cell is 20x20 pixels
+export const GAME_WIDTH  = 800;
+export const GAME_HEIGHT = 600;
+
+// Derived constants for the grid-based system
+export const BOARD_WIDTH = GAME_WIDTH / GRID_SIZE;
+export const BOARD_HEIGHT = GAME_HEIGHT / GRID_SIZE;
+
+export const FOOD_COLOR = 0xff0000;
+export const GAME_COLOR = 0x1d1d1d;
